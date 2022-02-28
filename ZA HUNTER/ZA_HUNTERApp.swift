23de2f0eb7f25b6e2160776c88bc343d4711813f
Peter Shrader <pshrader@bsd220.org>
2022-02-28 10:33:30 -0600

    Initial Commit

---
+++ ZA HUNTER/ZA_HUNTERApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZA_HUNTERApp.swift
+//  ZA HUNTER
+//
+//  Created by Student on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ZA_HUNTERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
